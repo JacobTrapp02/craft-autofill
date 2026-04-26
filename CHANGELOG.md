73f1f80256b3db2fe58727b83eb7c1c6c03648b8
Jake Trapp <jacob.trapp@colepublishing.com>
2026-04-26 10:34:33 -0500

    Init - Craft Plugin Generator

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Autofill
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jtdev\craftautofill\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Autofill settings
+ */
+class Settings extends Model
+{
+}
